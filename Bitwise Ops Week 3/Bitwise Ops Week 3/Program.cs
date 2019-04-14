d365824095d303fd5b721ebe875a59c9926483d2
GriffinG1 <4n0nym0ush4ck3r666@gmail.com>
2019-04-14 01:56:38 -0600

    Cleanup

--- Bitwise Ops Week 3/Bitwise Ops Week 3/Program.cs
+++ Bitwise Ops Week 3/Bitwise Ops Week 3/Program.cs
@@ -10,13 +10,13 @@ namespace Bitwise_Ops_Week_3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to an exercise in C# Bitwise Operators");
+            Console.WriteLine("Welcome to an exercise in C# Bitwise Operators.");
             Console.WriteLine();
             // This program will take two integer inputs and use bitwise operators for output.
-            Console.Write("Enter integer for x: ");
+            Console.Write("Enter a number for x: ");
             string xInt = Console.ReadLine();
             int x = Convert.ToInt32(xInt);
-            Console.Write("Enter integer for y: ");
+            Console.Write("Enter a number for y: ");
             string yInt = Console.ReadLine();
             int y = Convert.ToInt32(yInt);
             int AND = x & y;
@@ -24,11 +24,11 @@ namespace Bitwise_Ops_Week_3
             int inclOR = x | y;
             // Compares two bits and generates a result of 1 if the bits are complementary; otherwise, it returns 0.
             int exclOR = x ^ y;
-            // Compares two bits and returns 1 if either of the bits are 1 and it gives 0 if both bits are 0 or 1.
+            // Compares two bits and returns 1 if either of the bits are 1, otherwise, it returns 0.
             Console.WriteLine();
-            Console.WriteLine($"The AND value is: {AND}");
-            Console.WriteLine($"The inclusive OR value is: {inclOR}");
-            Console.WriteLine($"The exclusive OR value is: {exclOR}");
+            Console.WriteLine($"The AND value of {xInt} and {yInt} is: {AND}");
+            Console.WriteLine($"The inclusive OR value of {xInt} and {yInt} is: {inclOR}");
+            Console.WriteLine($"The exclusive OR value of {xInt} and {yInt} is: {exclOR}");
             Console.WriteLine("Press any key to continue...");
             Console.ReadLine();
         }
